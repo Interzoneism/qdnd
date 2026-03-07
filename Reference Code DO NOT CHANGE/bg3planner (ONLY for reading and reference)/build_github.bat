56544e1c85ca446b523570e1add6178614a22046
Interzoneism <martinforslund.studio@gmail.com>
2026-03-07 11:58:49 +0100

    cleanup

--- Reference Code DO NOT CHANGE/bg3planner (ONLY for reading and reference)/build_github.bat
+++
@@ -1,4 +0,0 @@
-START /B /WAIT cmd /c "ng build --output-path docs --base-href ./"
-START /B /WAIT cmd /c "copy .\docs\index.html .\docs\404.html"
-START /B /WAIT cmd /c "copy .\CNAME .\docs\CNAME"
-@pause
